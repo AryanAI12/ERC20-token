ae015e9ee890c68bfaa05aba9c4114c2699cca0d
AryanAI12 <116496430+AryanAI12@users.noreply.github.com>
2024-03-14 00:00:06 +0530

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+contract MyToken is ERC20, Ownable {
+constructor(address initialOwner) 
+ERC20 ("MyToken", "MTK")
+Ownable (initialOwner)
+{}
+function mint (address to, uint256 amount) public onlyOwner {
+_mint (to, amount);
+}
+}
